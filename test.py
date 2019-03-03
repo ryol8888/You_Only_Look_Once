5986e47dadd53cba8d19aabe48e5791c0d9a249e
ryol8888 <ryol8888@naver.com>
2019-03-03 22:38:38 +0900

    fixed to bounding_boxes

--- test.py
+++ test.py
@@ -32,8 +32,8 @@ def test(params):
     with open(class_path) as f:
         class_list = f.read().splitlines()
 
-    objness_threshold = 0.3
-    class_threshold = 0.3
+    objness_threshold = 0.5
+    class_threshold = 0.9
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -51,18 +51,21 @@ def test(params):
     if USE_SUMMARY:
         summary(model, (3, 448, 448))
 
+    image_path = os.path.join(data_path, "JPEGImages")
+
     if not (datalist_path =='./'):
         root = next(os.walk(os.path.abspath(data_path)))[0]
         dir = next(os.walk(os.path.abspath(data_path)))[1]
-        files =[]
-        with io.open(datalist_path,encoding='utf8') as f:
-            for i in f.readlines():
-                files.append(i.splitlines()[0])
-            
+        files =['000001']
+        #with io.open(datalist_path,encoding='utf8') as f:
+        #    for i in f.readlines():
+        #        files.append(i.splitlines()[0])
         for idx in range(len(files)):
             files[idx] += '.jpg'
-    image_path = os.path.join(data_path, "JPEGImages")
-    root, dir, files = next(os.walk(os.path.abspath(image_path)))
+
+    else:
+        root, dir, files = next(os.walk(os.path.abspath(image_path)))
+
 
     for file in files:
         extension = file.split(".")[-1]
@@ -91,21 +94,27 @@ def test(params):
         # INFERENCE
         outputs = model(input_img)
         b, w, h, c = outputs.shape
-
         outputs = outputs.view(w, h, c)
         outputs_np = outputs.cpu().data.numpy()
+        print("outputs_np : {}".format(outputs_np.shape))
 
-        objness = outputs[:, :, 0].unsqueeze(-1).cpu().data.numpy()
+        objness1 = outputs[:, :, 0].unsqueeze(-1).cpu().data.numpy()
+        objness2 = outputs[:, :, 5].unsqueeze(-1).cpu().data.numpy()
 
-        cls_map = outputs[:, :, 5:].cpu().data.numpy()
+        cls_map = outputs[:, :, 10:].cpu().data.numpy()
+
+
+        threshold_map1 = np.multiply(objness1, cls_map)
+        threshold_map2 = np.multiply(objness2, cls_map)
+        threshold_map = np.stack([threshold_map1,threshold_map2],axis=0)
+
+        objness = np.stack([objness1,objness2],axis=2)
+        objness = objness.reshape(7, 7, 2)
 
         print("obj : {}".format(objness.shape))
         print("cls : {}".format(cls_map.shape))
 
-        threshold_map = np.multiply(objness, cls_map)
-
         print("OBJECTNESS : {}".format(objness.shape))
-        print(objness)
         print("\n\n\n")
         print("CLS MAP : {}".format(cls_map.shape))
         print(cls_map[0])
@@ -117,48 +126,43 @@ def test(params):
         print("IMAGE SIZE")
         print("width : {}, height : {}".format(W, H))
         print("\n\n\n\n")
+        print("outputs_np : {}".format(outputs_np.shape))
 
         try:
 
             for i in range(7):
                 for j in range(7):
-                    draw.rectangle(((dx * i, dy * j), (dx * i + dx, dy * j + dy)), outline='#00ff88')
-
-                    if objness[i][j] >= objness_threshold:
-                        block = outputs_np[i][j]
-
-                        x_start_point = dx * i
-                        y_start_point = dy * j
-
-                        x_shift = block[1]
-                        y_shift = block[2]
-
-                        center_x = int((block[1] * W / 7.0) + (i * W / 7.0))
-                        center_y = int((block[2] * H / 7.0) + (j * H / 7.0))
-                        w_ratio = block[3]
-                        h_ratio = block[4]
-                        w_ratio = w_ratio * w_ratio
-                        h_ratio = h_ratio * h_ratio
-                        width = int(w_ratio * W)
-                        height = int(h_ratio * H)
-
-                        xmin = center_x - (width // 2)
-                        ymin = center_y - (height // 2)
-                        xmax = xmin + width
-                        ymax = ymin + height
-
-                        clsprob = block[5:] * objness[i][j]
-                        cls_idx = np.argmax(clsprob)
-
-                        if clsprob[cls_idx] > class_threshold:
-
-                            draw.rectangle(((xmin + 2, ymin + 2), (xmax - 2, ymax - 2)), outline="blue")
-                            draw.text((xmin + 5, ymin + 5), "{}: {:.2f}".format(class_list[cls_idx], clsprob[cls_idx]))
-                            draw.ellipse(((center_x - 2, center_y - 2),
-                                          (center_x + 2, center_y + 2)),
-                                         fill='blue')
-
+                    for k in range(2):
+                        draw.rectangle(((dx * i, dy * j), (dx * i + dx, dy * j + dy)), outline='#00ff88')
+                        if objness[i][j][k] >= objness_threshold:
+                            block = outputs_np[i][j]
+                            x_start_point = dx * i
+                            y_start_point = dy * j
+                            x_shift = block[1]
+                            y_shift = block[2]
+                            center_x = int((block[1] * W / 7.0) + (i * W / 7.0))
+                            center_y = int((block[2] * H / 7.0) + (j * H / 7.0))
+                            w_ratio = block[3]
+                            h_ratio = block[4]
+                            w_ratio = w_ratio * w_ratio
+                            h_ratio = h_ratio * h_ratio
+                            width = int(w_ratio * W)
+                            height = int(h_ratio * H)
+                            xmin = center_x - (width // 2)
+                            ymin = center_y - (height // 2)
+                            xmax = xmin + width
+                            ymax = ymin + height
+                            clsprob = block[10:] * objness[i][j][k]
+                            cls_idx = np.argmax(clsprob)
+                            if clsprob[cls_idx] > class_threshold:
+                                draw.rectangle(((xmin + 2, ymin + 2), (xmax - 2, ymax - 2)), outline="blue")
+                                draw.text((xmin + 5, ymin + 5), "{}: {:.2f}".format(class_list[cls_idx], clsprob[cls_idx]))
+                                draw.ellipse(((center_x - 2, center_y - 2),
+                                              (center_x + 2, center_y + 2)),
+                                             fill='blue')
+    
                         # LOG
+                        '''
                         print("idx : [{}][{}]".format(i, j))
                         print("x shift : {}, y shift : {}".format(x_shift, y_shift))
                         print("w ratio : {}, h ratio : {}".format(w_ratio, h_ratio))
@@ -168,7 +172,7 @@ def test(params):
                         print("width : {} height : {}".format(width, height))
                         print("class list : {}".format(class_list))
                         print("\n\n\n")
-
+                        '''
             plt.figure(figsize=(24, 18))
             plt.imshow(img)
             plt.show()
